3ac6c83f3f338a5b4079a9004b6295a811eb8a9b
kouta-takagi <k-takagi@ruby-dev.jp>
2024-10-31 13:33:07 +0900

    cart_itemのcreateとupdateを実装

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,17 +5,20 @@ class CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    if @cart_item.save
-      @cart_items = CartItem.includes(:product)
-      render json: @cart_items, include: :product, status: :created
+    if @cart_item = CartItem.find_by(product_id: params[:product_id])
+      @cart_item.quantity += params[:quantity].to_i
     else
-      render json: @cart_item.errors, status: :unprocessable_entity
+      @cart_item = CartItem.new(cart_item_params)
+    end
+    if @cart_item.save
+      render status: :created
+    else
+      render status: :unprocessable_entity
     end
   end
 
   def update
-    @cart_item = CartItem.find(params[:id])
+    @cart_item = CartItem.find(params[:product_id])
     if @cart_item.update(cart_item_params)
       @cart_items = CartItem.includes(:product)
       render json: @cart_items, include: :product
@@ -31,6 +34,6 @@ class CartItemsController < ApplicationController
 
   private
   def cart_item_params
-    params.require(:cart_item).permit(:quantity)
+    params.require(:cart_item).permit(:quantity, :product_id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
 
-  has_many :cart_items, dependent: :destroy
+  has_one :cart_items, dependent: :destroy
 end
